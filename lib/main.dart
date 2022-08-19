b0e51932dca10018590e60b583d7af72420a6fa6
Raihan-Sharif <raihan.raju@gmail.com>
2022-08-19 12:45:22 +0600

    score added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:my_first_app/quiz.dart';
-import 'package:my_first_app/result.dart';
 
 import './quiz.dart';
 import './result.dart';
@@ -18,23 +16,42 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var _questionList = const [
+  final _questionList = const [
     {
       'Question': 'What is your favourite Color?',
-      'Answer': ['Red', 'Black', 'White', 'Blue']
+      'Answer': [
+        {'text': 'Red', 'score': 4},
+        {'text': 'Black', 'score': 5},
+        {'text': 'White', 'score': 1},
+        {'text': 'Blue', 'score': 3},
+      ]
     },
     {
       'Question': 'What is your favourite Sports?',
-      'Answer': ['Cricket', 'Football', 'Tennis', 'BusketBall']
+      'Answer': [
+        {'text': 'Kabadi', 'score': 4},
+        {'text': 'Wrestling', 'score': 5},
+        {'text': 'Footbal', 'score': 1},
+        {'text': 'Cricket', 'score': 3},
+      ]
     },
     {
       'Question': 'What is your favourite Flower?',
-      'Answer': ['Rose', 'Merrygold', 'Jasmine', 'Daisy']
+      'Answer': [
+        {'text': 'Jui', 'score': 4},
+        {'text': 'Gondoraj', 'score': 5},
+        {'text': 'Rose', 'score': 1},
+        {'text': 'Belly', 'score': 3},
+      ]
     },
   ];
 
   var _questionIndex = 0;
-  void _answeredQuestion() {
+  var _scoreResult = 0;
+
+  void _answeredQuestion(int score) {
+    _scoreResult += score;
+
     setState(() {
       _questionIndex++;
       print(_questionIndex);
@@ -54,7 +71,7 @@ class _MyAppState extends State<MyApp> {
         ),
         body: _questionIndex < _questionList.length
             ? Quiz(_questionList, _questionIndex, _answeredQuestion)
-            : Result(),
+            : Result(_scoreResult),
       ),
     );
   }
